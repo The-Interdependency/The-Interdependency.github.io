abd0fbe0ad87eb560db412716def7571afd055bf
Erin Spencer <wayseer00@gmail.com>
2026-05-22 23:46:31 -0700

    build: add Eleventy config with collections and pass-through assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,23 @@
-import markdownIt from 'markdown-it';
-
-export default function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ 'assets': 'assets' });
-
-  const md = markdownIt({
-    html: true,
-    linkify: true,
-    typographer: true
-  });
-  eleventyConfig.setLibrary('md', md);
+module.exports = function(eleventyConfig) {
+  // Pass-through copy
+  eleventyConfig.addPassthroughCopy({ "assets": "assets" });
 
   // Collections
-  eleventyConfig.addCollection('articles', col => col.getFilteredByGlob('src/articles/**/*.md'));
-  eleventyConfig.addCollection('etiquette', col => col.getFilteredByGlob('src/etiquette/**/*.md'));
-  eleventyConfig.addCollection('addenda', col => col.getFilteredByGlob('src/addenda/**/*.md'));
-  eleventyConfig.addCollection('interdefinables', col => col.getFilteredByGlob('src/interdefinables/**/*.md'));
-  eleventyConfig.addCollection('lab', col => col.getFilteredByGlob('src/lab/**/*.md'));
-  eleventyConfig.addCollection('contra', col => col.getFilteredByGlob('src/contra/**/*.md'));
-  eleventyConfig.addCollection('glossary', col => col.getFilteredByGlob('src/glossary/**/*.md'));
-  eleventyConfig.addCollection('resources', col => col.getFilteredByGlob('src/resources/**/*.md'));
-  eleventyConfig.addCollection('changelog', col => col.getFilteredByGlob('src/changelog/**/*.md'));
+  eleventyConfig.addCollection("articles", c => c.getFilteredByGlob("src/articles/**/*.md"));
+  eleventyConfig.addCollection("etiquette", c => c.getFilteredByGlob("src/etiquette/**/*.md"));
+  eleventyConfig.addCollection("addenda", c => c.getFilteredByGlob("src/addenda/**/*.md"));
+  eleventyConfig.addCollection("interdefinables", c => c.getFilteredByGlob("src/interdefinables/**/*.md"));
+  eleventyConfig.addCollection("lab", c => c.getFilteredByGlob("src/lab/**/*.md"));
+  eleventyConfig.addCollection("contra", c => c.getFilteredByGlob("src/contra/**/*.md"));
 
   return {
     dir: {
-      input: 'src',
-      includes: '_includes',
-      data: '_data',
-      output: '_site'
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
     },
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    templateFormats: [ 'md', 'njk', 'html' ]
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
   };
-}
+};
